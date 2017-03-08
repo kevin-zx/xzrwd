6e06bd9d993cd91a7d1f7cb5aefaafc8dd6a9609
kevin-zx <519754821@qq.com>
2017-03-08 23:34:29 +0800

    auto commit

--- index.php
+++ index.php
@@ -3,8 +3,10 @@
 <?php
 	if (have_posts()) {
 		while (have_posts()) {
-			the_title();
-			the_post();
+			?>
+			<a href=<?php the_permalink(); ?>><?php the_title(); ?></a>
+			<?php
+			// the_post();
 		}
 	}else{
 		echo "have nothing post";
