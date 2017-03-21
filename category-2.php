ab680f6aff3564b7788427323fb7222614774080
kevin-zx <519754821@qq.com>
2017-03-21 22:26:14 +0800

    auto commit

--- category-2.php
+++ category-2.php
@@ -2,6 +2,6 @@
 $args = array ( 'category' => ID, 'posts_per_page' => 5);
 $myposts = get_posts( $args );
 foreach( $myposts as $post ) :	setup_postdata($post);
-var_dump($post);
+	the_content();
  ?>
 <?php endforeach; ?>
